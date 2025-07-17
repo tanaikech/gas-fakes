8a3bd2bc7ea2b2b99ec927b0c0e57f0afb84bbed
Bruce Mcpherson <bruce@mcpher.com>
2025-07-17 12:10:24 +0100

    gasmess

---
+++ gasmess/bruce/basic.js
@@ -0,0 +1,25 @@
+import '../../main.js';
+
+// put all this stuff in a temp folder for easy deletion
+const folderName = "---gasmess-bruce"
+const folders = DriveApp.getFoldersByName (folderName)
+let folder = null
+if (folders.hasNext()) {
+  folder = folders.next()
+  console.log ('using existing temp folder', folder.getName(), folder.getId())
+} else {
+  folder = DriveApp.createFolder (folderName)
+  console.log ('created temp folder', folder.getName(), folder.getId())
+}
+
+const ss = SpreadsheetApp.create("--gasmess-sheet")
+const file = DriveApp.getFileById(ss.getId())
+const sheet = ss.insertSheet()
+const values = [[1,2,3],[4,5,6],[7,8,9]]
+const range = sheet.getRange(1,1,3,3)
+range.setValues(values)
+
+const result = range.getValues()
+console.log (JSON.stringify(result) === JSON.stringify(values))
+
+file.setTrashed(true)
