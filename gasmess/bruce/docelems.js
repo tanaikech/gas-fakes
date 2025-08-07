113b38f9b814e57f1afcaa34ef8028da055a6410
Bruce Mcpherson <bruce@mcpher.com>
2025-08-07 11:05:18 +0100

    stash

--- gasmess/bruce/docelems.js
+++ gasmess/bruce/docelems.js
@@ -1,26 +1,50 @@
 import '../../main.js';
 import { moveToTempFolder, deleteTempFile } from './tempfolder.js';
-const show = (doc, what = "--") => {
-  console.log(what)
-  const body = doc.getBody()
-  console.log('number of children', body.getNumChildren())
-  const children = []
-  for (let n = 0; n < body.getNumChildren(); n++) children.push(body.getChild(n))
-  console.log('children', children.map(f => ({ type: f.getType().toString(), text: f.getText() })))
-  console.log('body get text', body.getText())
-  if (!DocumentApp.isFake) {
-    doc.saveAndClose()
-    doc = DocumentApp.openById(doc.getId())
+const pb1 = () => {
+
+  const getChildren= (body) => {
+    const children = []
+    for (let c = 0; c < body.getNumChildren() ; c++) {
+      children.push (body.getChild(c))
+    }
+    return children
   }
-  const adoc = Docs.Documents.get(doc.getId())
-  console.log(JSON.stringify(adoc.body.content.map(f => ({ keys: Object.keys(f).filter(g => !g.startsWith('__')), startIndex: f.startIndex, endIndex: f.endIndex, elements: (f.paragraph?.elements || []).map(g => ({ keys: Object.keys(g).filter(g => !g.startsWith('__')), startIndex: g.startIndex, endIndex: g.endIndex, textRun: g.textRun?.content })) }))))
-  return doc
+
+  const show = (doc, what = "--") => {
+    console.log(what)
+    const body = doc.getBody()
+    console.log('number of children', body.getNumChildren())
+    const children = []
+    for (let n = 0; n < body.getNumChildren(); n++) children.push(body.getChild(n))
+    console.log('children', children.map(f => ({ type: f.getType().toString(), text: f.getText() })))
+    console.log('body get text', body.getText())
+    if (!DocumentApp.isFake) {
+      doc.saveAndClose()
+      doc = DocumentApp.openById(doc.getId())
+    }
+    const adoc = Docs.Documents.get(doc.getId())
+    console.log(JSON.stringify(adoc.body.content.map(f => ({ keys: Object.keys(f).filter(g => !g.startsWith('__')), startIndex: f.startIndex, endIndex: f.endIndex, elements: (f.paragraph?.elements || []).map(g => ({ keys: Object.keys(g).filter(g => !g.startsWith('__')), startIndex: g.startIndex, endIndex: g.endIndex, textRun: g.textRun?.content })) }))))
+
+
+    return doc
+  }
+
+  let doc = DocumentApp.create("pb");
+  moveToTempFolder(doc.getId())
+  doc = show(doc, '\n...empty document')
+
+  const p1 = doc.getBody().appendParagraph("p1")
+  doc = show(doc, '\n...p1')
+
+  const p2 = doc.getBody().insertParagraph(0, "p0")
+  doc = show(doc, '\n...inserted p0')
+  
+  const children = getChildren(doc.getBody())
+  const [c1,c2] = children
+  c1.appendPageBreak ()
+  doc = show(doc, 'inserted pb after p0')
+
+  deleteTempFile(doc.getId())
+
 }
-
-let doc = DocumentApp.create("pb");
-moveToTempFolder(doc.getId())
-doc = show(doc, '\n...empty document')
-const p1 = doc.getBody().appendParagraph("p1")
-doc = show(doc, 'appended p1')
-
-deleteTempFile(doc.getId())
+pb1()
---
+++ gasmess/bruce/pb4.js
@@ -0,0 +1,69 @@
+import '../../main.js';
+import { moveToTempFolder, deleteTempFile } from './tempfolder.js';
+
+const whichType = (element) => {
+  const ts = ["paragraph", "pageBreak", "textRun"]
+  const [t] = ts.filter(f => Reflect.has(element, f))
+  if (!t) console.log('skipping element', element)
+  return t
+}
+const report = (doc, what) => {
+  const body = doc.body
+  // drop the section break
+  const children = body.content.slice(1)
+  what += ` -children:${children.length}`
+  console.log (what)
+  let text = '  '
+  const typer = (child, text) => {
+    const type = whichType(child)
+    if (type) {
+      text += ` -type:${type} ${child.startIndex}:${child.endIndex}`
+      if (type === 'textRun') {
+        text += ` -text:${JSON.stringify(child[type].content)}`
+      }
+      if (Reflect.has(child[type], "elements")) {
+        text += ` (`
+        child[type].elements.forEach(f => text = typer(f, text))
+        text += ')'
+      }
+    }
+    return text
+  }
+  return children.map(f => typer(f, text)).join("\n")
+}
+const pb4 = () => {
+
+  let doc = DocumentApp.create("abc")
+  const id = doc.getId()
+  moveToTempFolder(id)
+  const sc = () => {
+    if (!DocumentApp.isFake) {
+      doc.saveAndClose()
+      doc = DocumentApp.openById(id)
+    }
+  }
+
+  sc()
+  console.log(report(Docs.Documents.get(id), `\nEmpty Document`))
+
+  doc.getBody().appendParagraph ("p1")
+  sc()
+  console.log(report(Docs.Documents.get(id), `\nAppend p1 to empty doc`))
+  
+  doc.getBody().appendParagraph ("p2")
+  sc()
+  console.log(report(Docs.Documents.get(id), `\nAppend p2 after p1`))
+
+  doc.getBody().insertParagraph (2,"p1a")
+  sc()
+  console.log(report(Docs.Documents.get(id), `\ninsert para between 1 and 2`))
+
+  doc.getBody().appendPageBreak ()
+  sc()
+  console.log(report(Docs.Documents.get(id), `\nappend page break to end of document`))
+
+  deleteTempFile(id)
+
+}
+
+pb4()
