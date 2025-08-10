93b1688238f5f58a67c1f1c3d07dd1e81643f334
Bruce Mcpherson <bruce@mcpher.com>
2025-08-10 15:59:51 +0100

    whew

--- gasmess/bruce/pb4.js
+++ gasmess/bruce/pb4.js
@@ -46,32 +46,39 @@ const pb4 = () => {
   sc()
   console.log(report(Docs.Documents.get(id), `\nEmpty Document`))
 
-  doc.getBody().appendParagraph("p1")
+  doc.getBody().appendParagraph ("p1")
   sc()
   console.log(report(Docs.Documents.get(id), `\nAppend p1 to empty doc`))
-
-  doc.getBody().appendParagraph("p2")
+  
+  doc.getBody().appendParagraph ("p2")
   sc()
   console.log(report(Docs.Documents.get(id), `\nAppend p2 after p1`))
 
-  doc.getBody().insertParagraph(2, "p1a")
+  doc.getBody().insertParagraph (2,"p1a")
   sc()
   console.log(report(Docs.Documents.get(id), `\ninsert para between 1 and 2`))
 
-  doc.getBody().appendPageBreak()
+  doc.getBody().appendPageBreak ()
   sc()
   console.log(report(Docs.Documents.get(id), `\nappend page break to end of document`))
 
   const p1a = doc.getBody().getChild(2)
-  p1a.appendPageBreak()
+  p1a.appendPageBreak ()
   sc()
   console.log(report(Docs.Documents.get(id), `\nappend page break to end of of para 1a`))
-
 
   doc.getBody().appendParagraph ("p3")
   sc()
   console.log(report(Docs.Documents.get(id), `\nappend paragraph p3 to body`))
 
+  const pb1 = doc.getBody().appendPageBreak().copy()
+  //doc.getBody().insertPageBreak(4,pb1)
+  sc()
+  console.log(report(Docs.Documents.get(id), `\nappend pagebreak to body`))
+  
+  doc.getBody().insertPageBreak(4,pb1)
+  sc()
+  console.log(report(Docs.Documents.get(id), `\ninsert pb to body before p3`))
 
   deleteTempFile(id)
 
