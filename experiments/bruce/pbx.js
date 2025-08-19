24e89e57d50ed3949631ed467bef800edd5247ff
Bruce Mcpherson <bruce@mcpher.com>
2025-08-19 11:40:19 +0100

    stash

--- experiments/bruce/pbx.js
+++ experiments/bruce/pbx.js
@@ -56,9 +56,62 @@ const pbnew = () => {
   moveToTempFolder(id, suffix)
 
   let body = doc.getBody()
-  body.appendParagraph('foo')
+  body.appendParagraph('para append')
   doc = scl(doc)
-  console.log(report(Docs.Documents.get(id), `\nappended para`))
+  console.log(report(Docs.Documents.get(id), `\n1.appended para`))
+
+  body = doc.getBody()
+  body.insertParagraph(0, 'para 0')
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n2.inserted para 0`))
+
+  body = doc.getBody()
+  body.insertParagraph(2, 'para 2 insert')
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n3.inserted para 2`))
+
+  body = doc.getBody()
+  body.insertParagraph(2, 'para 2 insert')
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n4.inserted para 2`))
+
+  body = doc.getBody()
+  body.appendPageBreak()
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n5.appended pagebreak to body after para2 insert`))
+
+  body = doc.getBody()
+  let c = body.getChild(2)
+  c.appendText( 'p2 appended text')
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n6.appended text to para 2`))
+
+  body = doc.getBody()
+  c = body.getChild(2)
+  c.appendPageBreak()
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n7.appended pagebreak to  para 2`))
+
+  body = doc.getBody()
+  body.insertParagraph(3, 'para 3 insert')
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n8.inserted para 2`))
+
+  body = doc.getBody()
+  body.insertPageBreak(3)
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n9.inserted page break in para 3`))
+
+  body = doc.getBody()
+  c=body.getChild(3)
+  c.appendText('after pagebreak in 3')
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n10.inserted text after pagebreak in 3`))
+
+  body = doc.getBody()
+  body.appendPageBreak()
+  doc = scl(doc)
+  console.log(report(Docs.Documents.get(id), `\n11.appended pagebreak to body at end`))
 
   deleteTempFile(id)
 }
