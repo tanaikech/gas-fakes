5f2acffc1bc1f010b37e386d92706c827326f751
tanaikech <tanaike@hotmail.com>
2025-08-20 15:44:10 +0900

    Add methods to fakesheet.js

--- gasmess/tanaike/for_simple_test.js
+++ gasmess/tanaike/for_simple_test.js
@@ -4,11 +4,3 @@ import { moveToTempFolder, deleteTempFile } from "./tempfolder.js";
 
 const spreadsheet = SpreadsheetApp.openById("");
 const sheet = spreadsheet.getSheets()[0];
-const data = ["sample1", "sample2", "sample3"];
-sheet.appendRow(data).getSheetId();
-const a = [sheet.getLastRow(), 1];
-const b = [sheet.getLastColumn(), data.length];
-const c = [sheet.getRange(1, 1, 1, data.length).getValues(), [data]];
-console.log(a);
-console.log(b);
-console.log(c);
