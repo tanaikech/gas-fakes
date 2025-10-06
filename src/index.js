a566e6e4d7f6b7d0ba13d4eae72b2f0b09b3c6e8
bruce mcpherson <bruce@mcpher.com>
2025-10-06 17:10:05 +0100

    npm-fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import colors from 'yoctocolors'
 import './services/scriptapp/app.js'
 
-import {default as pf } from '../package.json' with { type: 'json' };
-console.log (`You are using ${colors.blueBright(pf.name)} package version ${colors.greenBright(pf.version)}`)
-ScriptApp.__gasFakesVersion = pf.version
 import './services/driveapp/app.js'
 import './services/logger/app.js'
 import './services/urlfetchapp/app.js'
@@ -21,3 +17,4 @@ import './services/documentapp/app.js'
 import './services/advforms/app.js'
 import './services/formapp/app.js'
 import './services/slidesapp/app.js'
+ScriptApp.isFake
