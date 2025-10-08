737c361a669d8818aeeadf8a6121262fe234ede7
bruce mcpherson <bruce@mcpher.com>
2025-10-08 11:05:04 +0100

    adc-changes

---
+++ shells/enable.sh
@@ -0,0 +1,2 @@
+# enables services supported by gas-fakes
+gcloud services enable drive.googleapis.com sheets.googleapis.com forms.googleapis.com docs.googleapis.com gmail.googleapis.com logging.googleapis.com
