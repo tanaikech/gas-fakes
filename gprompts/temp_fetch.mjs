0248cd2480dd27e27a0f270867818addf9292a46
bruce mcpherson <bruce@mcpher.com>
2025-10-14 10:38:54 +0100

    cleanup

--- gprompts/temp_fetch.mjs
+++
@@ -1,9 +0,0 @@
-import got from 'got';
-
-const url = 'https://developers.google.com/apps-script/reference/document';
-
-got(url).then(response => {
-  console.log(response.body);
-}).catch(err => {
-  console.error(err);
-});
