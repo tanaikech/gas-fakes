9702b1acd62fec9c9eeb0326fc9f564895e54cee
bruce mcpherson <bruce@mcpher.com>
2026-03-03 12:27:56 +0000

    clean

--- gasmess/tanaike/for_simple_test.js
+++
@@ -1,7 +0,0 @@
-import "../../main.js";
-
-ScriptApp.__behavior.sandBoxMode = true;
-const spreadsheet = SpreadsheetApp.create("sample1");
-const sheet = spreadsheet.getSheets()[0];
-sheet.setName("sample");
-ScriptApp.__behavior.trash();
